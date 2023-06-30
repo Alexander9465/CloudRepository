91fbde089c3ace576e3cd5d3c9def3ab8bd5bf58
Alexander9465 <alexander94_6@hotmail.com>
2023-06-30 13:17:29 +0200

    try testing

--- SpringCloudPrueba-1/src/main/java/com/cloudcustomer/inicio/SpringCloudPrueba1Application.java
+++ SpringCloudPrueba-1/src/main/java/com/cloudcustomer/inicio/SpringCloudPrueba1Application.java
@@ -3,11 +3,13 @@ package com.cloudcustomer.inicio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan(basePackages = "com.cloudcustomer.entity")
 @EnableJpaRepositories(basePackages = "com.cloudcustomer.repository")
 @SpringBootApplication(scanBasePackages = {"com.cloudcustomer.controller","com.cloudcustomer.service"})
+@EnableEurekaClient
 public class SpringCloudPrueba1Application {
 
 	public static void main(String[] args) {
